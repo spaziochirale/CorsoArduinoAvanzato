3379d066fc2317f1abf187a42c093c4eb81dda2c
Centro Ricerca e Sviluppo Spazio Chirale <46569915+spaziochirale@users.noreply.github.com>
2019-01-22 21:04:36 +0100

    Versione iniziale

---
+++ DemoEEPROM.ino
@@ -0,0 +1,39 @@
+/***************************************************
+ *    ESEMPIO SALVATAGGIO DELLO STATO USANDO LA EEPROM
+ *    Derivato dall'esempio Interrupt con la gestione del 'debounce'
+ *    
+ *    Author: Stefano Capezzone
+ *    Version: 1.0
+ *    Corso Professionale di Arduino Avanzato
+ *    
+ */
+#include <EEPROM.h>
+const byte BUTTON = 2; // Modificare con il PIN scelto anche in base al tipo di scheda
+volatile byte stato = 1;
+volatile unsigned long tempo;
+
+
+
+void buttonPressed() {
+  if ((millis() - tempo) > 200) 
+    {
+      stato = !stato;
+      EEPROM.update(10,stato);
+    }
+  digitalWrite(LED_BUILTIN, stato);  
+  tempo = millis();
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, RISING);
+  tempo = millis();
+  stato=EEPROM.read(10);
+  digitalWrite(LED_BUILTIN, stato);
+
+}
+
+void loop() {
+  
+  // Non ho nulla da fare...
+}
